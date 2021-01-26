07e769cd7379e1cd54fecf662e6564f33847d4e8
Logan Herzog <logan.herzog@gmail.com>
2021-01-26 20:44:00 +0000

    Added create action to save to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,7 @@ class ContactsController < ApplicationController
    
    def create
       @contact = Contact.new(contact_params)
+      
       if @contact.save
          redirect_to new_contact_path, notice: "Message sent."
       else
