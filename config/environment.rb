e7ecf022dc99fdf871ab6e1463fb8a78d54fe3f0
Logan Herzog <logan.herzog@gmail.com>
2021-01-28 17:05:54 +0000

    Fixed typos in environment file

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,9 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.stmp_settings = {
+ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain         => 'https://agile-bayou-85399.herokuapp.com/',
