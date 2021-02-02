e566c25716ae2884300aa5fd68dad79203a7fb78
Logan Herzog <logan.herzog@gmail.com>
2021-02-02 17:58:16 +0000

    Added code comments for Stripe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   
   before_action :configure_permitted_parameters, if: :devise_controller?
   
+  # Whitelist the following form fields so that we can process them if coming 
+  # from a Devise signup form.
   protected
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:stripe_card_token, :email, :password, :password_confirmation)}
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,19 @@ class User < ApplicationRecord
   
   attr_accessor :stripe_card_token
   
+  # If Pro user passes validations (email, password, etc.), then call Stripe
+  # and tell Stripe to set up a subscription upon charging a customer's card.
+  #
+  # NOTE: Subscription does not work due to difference in Stripe dashboard
+  # from when course was created. I took "plan" out of the params when creating
+  # the customer because I was unable to assign a plan ID to the different plans
+  # on my Stripe dashboard.
+  #
+  # Stripe responds back with customer data.
+  # Store customer.id as the customer token and save the user.
   def save_with_subscription
       if valid?
-          customer = Stripe::Customer.create(description: email, id: "prod_Iqj5x1aeQBG4J4", card: stripe_card_token)
+          customer = Stripe::Customer.create(description: email, card: stripe_card_token)
           self.stripe_customer_token = customer.id
           save!
       end
