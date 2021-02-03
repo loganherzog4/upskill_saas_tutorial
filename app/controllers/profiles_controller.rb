ae31c173a57834ac20e976d93108533d00389ca4
Logan Herzog <logan.herzog@gmail.com>
2021-02-03 17:48:01 +0000

    Added new user profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,35 @@
 class ProfilesController < ApplicationController
     
-    # GET to /users/:user_id/profile/new
+    # GET request to /users/:user_id/profile/new.
     def new
         
-        # Render blank profile details form
+        # Render blank profile details form.
         @profile = Profile.new
         
     end
+    
+    # POST request to /users/:user_id/profile.
+    def create
+        
+        # Ensure that we have the user who is filling out form.
+        @user = User.find(params[:user_id])
+        
+        # Create profile linked to this specific user.
+        @profile = @user.build_profile(profile_params)
+        
+        if @profile.save
+            flash[:success] = "Profile updated."
+            redirect_to root_path
+        else
+            render action: :new
+        end
+        
+    end
+    
+    private
+        # To collect data from form, we need to use strong parameters and whitelist the form fields.
+        def profile_params
+            params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+        end
     
 end
